6529db102d5f5e33d647e8db2c4ed9dbe83fa1f4
Paolo Giuliano <pgiuliano@downsviewdrywall.com>
2025-03-04 16:45:21 -0500

    Added Tailwind command to package.json

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <div className="h-24 w-24 border-5 border-solid border-blue-100">
-        <p className="text-red-500">Center This Element</p>
+        <p className="text-red-600">Center This Element</p>
       </div>
     </div>
   );
