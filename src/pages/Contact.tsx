8e3c8b1c315bbe9d964ca1ad321f7857dd252671
Paolo Giuliano <pgiuliano@downsviewdrywall.com>
2025-03-04 16:01:40 -0500

    Initial commit

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="h-24 w-24 border-5 border-solid border-blue-100">
+        <p className="text-red-500">Center This Element</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
